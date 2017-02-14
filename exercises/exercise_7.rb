43c6db418665a9e6c1cf522fcd9786591756c125
trrippy <wes.wong@hotmail.com>
2017-02-14 22:26:53 +0000

    asks user for store store and shows erros

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "store name "
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+
+puts "#{new_store.errors.details.inspect}"
+# print "employee first name "
+# employee_name = gets.chomp
+# new_employee = @store1.employees.create(first_name: employee_name)
+# puts "#{new_employee.errors.details.inspect}"
