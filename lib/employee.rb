0e427dc5e0b4cd9168c700b98a13060f9109d1d0
trrippy <wes.wong@hotmail.com>
2017-02-14 22:27:30 +0000

    validating employee fields 🤷🏻‍♂️

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,16 @@
 class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name,
+    presence: true
+
+  validates :last_name,
+    presence: true
+
+  validates :hourly_rate,
+    numericality: { greater_than: 40, less_than: 200 }
+
+
+  validates :store_id,
+    presence:true
 
 end
