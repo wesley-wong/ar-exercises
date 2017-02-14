2d9f9f3f95f3419de068206ca676cbae199a361b
trrippy <wes.wong@hotmail.com>
2017-02-14 20:14:39 +0000

    created 3 stores, and puts storecount ✨

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,15 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+
 # Your code goes below here ...
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+#  Burnaby (annual_revenue of 300000, carries men’s and women’s apparel)
+# * Richmond (annual_revenue of 1260000 carries women’s apparel only)
+# * Gastown (annual_revenue of 190000 carries men’s apparel only)
+puts "Store count is #{Store.count}"
