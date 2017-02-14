43d54e333f50be6eeb853d85971bd28420f6ca97
trrippy <wes.wong@hotmail.com>
2017-02-14 21:22:03 +0000

    creates 3 more stores and puts mens stores and womens stores with annual rev of less than a mil 🐶

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,17 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name:"Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name:"Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name:"Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |item|
+  puts "#{item.name} has an annual revenue of #{item.annual_revenue}"
+end
+
+@womens_stores_less_than_million = Store.where(womens_apparel: true).where('annual_revenue < 1000000', true)
+@womens_stores_less_than_million.each do |item|
+  puts "#{item.name} has an annual revenue of #{item.annual_revenue}"
+end
