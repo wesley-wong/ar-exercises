4a70aeac85545f6e61fd8d7db7d84732ad0e09ad
trrippy <wes.wong@hotmail.com>
2017-02-14 21:43:31 +0000

    puts sum reveneue, store avg, and #of stores with >1million sales

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum = Store.sum(:annual_revenue)
+p "The sum is $#{sum}"
+
+avg = Store.average(:annual_revenue)
+puts avg
+
+big_stores = Store.where('annual_revenue < 1000000', true).count
+# big_stores.each do |item|
+#   puts "#{item.name} has an annual revenue of #{item.annual_revenue}"
+# end
+puts "There are #{big_stores} which have more than $1 million in sales"
