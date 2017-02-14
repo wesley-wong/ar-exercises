5f59f19e773fca5e86f6e8d0f89e6fc9bd0fb071
trrippy <wes.wong@hotmail.com>
2017-02-14 22:26:26 +0000

    adds employees to stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Wesley", last_name: "Wong", hourly_rate: 35)
+@store2.employees.create(first_name: "Andrew", last_name: "Mandrew", hourly_rate: 40)
+@store2.employees.create(first_name: "Steven", last_name: "Even", hourly_rate: 35)
